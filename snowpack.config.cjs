e3d8ba25a22e574d3b0e42c6088da49050d4efbb
Garrett Brown <garrett.brown@aclima.io>
2021-09-01 17:32:00 -0700

    Add snowpack build system

---
+++ snowpack.config.cjs
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2020 Aclima, Inc.
+ * This file is part of iot2web - https://github.com/Aclima/iot2web.git
+ */
+
+/* eslint @typescript-eslint/no-var-requires: "off" */
+
+const globals = require("rollup-plugin-node-globals");
+const polyfills = require("rollup-plugin-node-polyfills");
+const resolve = require("@rollup/plugin-node-resolve").nodeResolve;
+
+module.exports = {
+  mount: {},
+  devOptions: {
+    bundle: false,
+  },
+  buildOptions: {
+    minify: false,
+  },
+  plugins: ["@snowpack/plugin-typescript"],
+  optimize: {
+    treeshake: true,
+  },
+  packageOptions: {
+    sourceMap: true,
+    knownEntrypoints: [],
+    rollup: {
+      plugins: [
+        // Fix "Uncaught TypeError: bufferEs6.hasOwnProperty is not a function"
+        resolve({
+          preferBuiltins: false,
+        }),
+        globals(),
+        polyfills(),
+      ],
+    },
+  },
+};
