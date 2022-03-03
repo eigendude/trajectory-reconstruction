c9a38c3b19793f451686b02dafb7240feddc89d0
Garrett Brown <eigendebugger@gmail.com>
2022-03-03 11:54:49 -0800

    Add Mocha/Chai test framework
    Mocha's TypeScript support is poor. In the past I've used ts-mocha,
    which is a thin TypeScript wrapper around Mocha, but that doesn't
    handle ES6 modules, and I want to stay away from CommonJS.
    
    To solve this, Babel is used to compile TypeScript into JavaScript
    for Mocha at runtime.

---
+++ test/test-mocha.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2020 Garrett Brown
+ * This file is part of trajectory-reconstruction - https://github.com/eigendude/trajectory-reconstruction
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * See LICENSE.txt for more information.
+ */
+
+/* eslint @typescript-eslint/no-unused-expressions: "off" */
+
+import chai from "chai";
+
+describe("Mocha", function () {
+  it("should perform a basic test", async function () {
+    chai.expect(true).to.be.true;
+  });
+});
