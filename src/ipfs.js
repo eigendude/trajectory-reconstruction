90a2e9965552f4996271609e288fc0bf685429e7
Garrett Brown <eigendebugger@gmail.com>
2024-02-16 16:55:44 -0800

    Remove Infura IPFS gateway

--- src/ipfs.js
+++ src/ipfs.js
@@ -7,14 +7,10 @@
  */
 
 // IPFS parameters
-const IPFS_GATEWAYS = [
-  "https://ipfs.io",
-  "https://ipfs.infura.io",
-  "https://gateway.ipfs.io",
-];
+const IPFS_GATEWAYS = ["https://ipfs.io", "https://gateway.ipfs.io"];
 
 // TODO
-//const IPFS_GATEWAY = IPFS_GATEWAYS[Math.floor(Math.random() * IPFS_GATEWAYS.length)];
-const IPFS_GATEWAY = IPFS_GATEWAYS[1];
+const IPFS_GATEWAY =
+  IPFS_GATEWAYS[Math.floor(Math.random() * IPFS_GATEWAYS.length)];
 
 export { IPFS_GATEWAY };
